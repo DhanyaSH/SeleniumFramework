217853ce681adf53dc2ef3ad2b20250ed71e67da
DhanyaSH <dhanyacm25@gmail.com>
2024-03-10 16:14:41 +0400

    creating framework

--- SeleniumFramework/src/test/java/com/utility/BrowserUtility.java
+++ SeleniumFramework/src/test/java/com/utility/BrowserUtility.java
@@ -1,19 +1,54 @@
 package com.utility;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+public abstract class BrowserUtility {
+	
+	private WebDriver wd;
 
-
-public class BrowserUtility {
-private WebDriver wd;
-
-public BrowserUtility(WebDriver wd) {
-	super();
-	this.wd = wd;
-}
+	public BrowserUtility(WebDriver wd) {
+		super();
+		this.wd = wd;
+	}
 
 	public void goToWebsite(String url) {
-	wd.get("https://freecrm.com/");
+		wd.get(url);
+
+	}
+
+	public void viewInfullScreen() {
+
+		wd.manage().window().maximize();
+
+	}
+
+	public void enterText(By Locator, String textToEnter) {
+		WebElement userNameTextBoxWebElement = wd.findElement(Locator);
+		userNameTextBoxWebElement.clear();
+		userNameTextBoxWebElement.sendKeys();
+	}
+
+	public void clickOn(By locator) {
+		WebElement userNameTextBoxWebElement = wd.findElement(locator);
+		userNameTextBoxWebElement.click();
+		
+	}
+	public void sleepFor(int seconds) {
 	
-}
-}
+	try {
+		Thread.sleep(seconds*1000);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	}
+		
+		
+}	
+		
+	
+	
+	
