4a3cd0560c2e9357770e664b8a871de33c3df88d
DhanyaSH <dhanyacm25@gmail.com>
2024-03-29 18:19:37 +0400

    testadded

--- SeleniumFramework/src/test/java/com/utility/BrowserUtility.java
+++ SeleniumFramework/src/test/java/com/utility/BrowserUtility.java
@@ -3,14 +3,21 @@ package com.utility;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.google.protobuf.Duration;
 
 public abstract class BrowserUtility {
-	
+
 	private WebDriver wd;
+
+	private WebDriverWait wait;
 
 	public BrowserUtility(WebDriver wd) {
 		super();
 		this.wd = wd;
+		wait = new WebDriverWait(wd, java.time.Duration.ofSeconds(30));
 	}
 
 	public void goToWebsite(String url) {
@@ -25,30 +32,35 @@ public abstract class BrowserUtility {
 	}
 
 	public void enterText(By Locator, String textToEnter) {
-		WebElement userNameTextBoxWebElement = wd.findElement(Locator);
+		WebElement userNameTextBoxWebElement = wait.until(ExpectedConditions.visibilityOfElementLocated(Locator));
 		userNameTextBoxWebElement.clear();
-		userNameTextBoxWebElement.sendKeys();
+		userNameTextBoxWebElement.sendKeys(textToEnter);
 	}
 
 	public void clickOn(By locator) {
-		WebElement userNameTextBoxWebElement = wd.findElement(locator);
+		WebElement userNameTextBoxWebElement = wait.until(ExpectedConditions.elementToBeClickable(locator));
 		userNameTextBoxWebElement.click();
-		
+
 	}
+
+	public String getVibileText(By locator) {
+
+		WebElement iconInloginpagElement = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+
+		String iconInloginpagElementText = iconInloginpagElement.getText();
+		return iconInloginpagElementText;
+
+	}
+
 	public void sleepFor(int seconds) {
-	
-	try {
-		Thread.sleep(seconds*1000);
-	} catch (InterruptedException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+
+		try {
+			Thread.sleep(seconds * 1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
-	
-	}
-		
-		
-}	
-		
-	
-	
-	
+
+}
